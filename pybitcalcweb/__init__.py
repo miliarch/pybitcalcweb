561c7230dc8aabc322d33ebfc914d9f71e7bbbc6
Marcus Bowman <miliarch.mb@gmail.com>
2017-09-24 01:02:01 -0700

    Updated decimal jinja2 filter

--- pybitcalcweb/__init__.py
+++ pybitcalcweb/__init__.py
@@ -6,12 +6,25 @@ def format_pretty_json(json_output):
     return dumps(json_output, indent=4, sort_keys=True)
 
 
+'''def format_decimal(amount):
+    return '{:0.3f}'.format(amount).rstrip("0").rstrip(".")'''
+
 def format_decimal(amount):
-    return '{:0.3f}'.format(amount).rstrip("0").rstrip(".")
+    amt_whole = int(amount)
+    amt_whole_len = len(str(amt_whole))
 
+    if amount < 1:
+        amt_str = '{:0.15f}'.format(amount).rstrip("0").rstrip(".")
+    elif amt_whole_len < 4:
+        amt_str = '{:0.3f}'.format(amount).rstrip("0").rstrip(".")
+    elif amt_whole_len < 6:
+        amt_str = '{:0.2f}'.format(amount).rstrip("0").rstrip(".")
+    elif amt_whole_len < 9:
+        amt_str = '{:0.1f}'.format(amount).rstrip("0").rstrip(".")
+    else:
+        amt_str = '{}'.format(amt_whole)
 
-def format_decimal_zero(amount):
-    return '{:0.15f}'.format(amount).rstrip("0").rstrip(".")
+    return amt_str
 
 
 # Create Flask app
@@ -21,7 +34,6 @@ app.config.from_object('config')
 # Define some jinja2 filters
 app.jinja_env.filters['pretty_json'] = format_pretty_json
 app.jinja_env.filters['decimal'] = format_decimal
-app.jinja_env.filters['decimal_zero'] = format_decimal_zero
 
 # Trim and lstrip blocks so HTML source is neat
 app.jinja_env.trim_blocks = True
